b7991d0ffa64572abd5634ba90ed74e9576ab528
RafiMoreno <ahmadarrafi.m@gmail.com>
2024-04-19 02:52:31 +0700

    feat: article list screen

---
+++ src/helpers/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const axiosClient  =  axios.create({
+    baseURL: `https://dummyapi.online/api`
+})
+  
+export const getAllArticles = () => {
+    return axiosClient.get(`/blogposts`)
+  }
