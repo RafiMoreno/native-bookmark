8c223dc885443d18c3aef084f9834e67d5de2d08
RafiMoreno <ahmadarrafi.m@gmail.com>
2024-04-19 01:39:32 +0700

    feat: add bottom navigation

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ArticleList from "./src/screens/ArticleList";
+import Bookmarks from "./src/screens/Bookmarks";
+import { StyleSheet } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ headerShown: false, tabBarActiveTintColor: "#f5b942" }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="article" color={color} size={size} />
+            ),
+          }}
+          name="Articles"
+          component={ArticleList}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="bookmark" color={color} size={size} />
+            ),
+          }}
+          name="Bookmarks"
+          component={Bookmarks}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
